59a132c1cc989c382f062fdd5ecc026ef2ff9d9b
jodi <jodijabr@gmail.com>
2025-05-18 13:23:17 +0300

    git files

--- app.js
+++ app.js
@@ -23,7 +23,84 @@ try {
     window.location.href = './signup-or-login.html'
 })
 } catch (error) {
-    console.log('ll');
     
 }
+
+try {
+    let toSignupPage = document.getElementById('to-signup-page')
+    toSignupPage.addEventListener('click',(e)=>{
+window.location.href = './signUp.html'
+})
+} catch (error) {
+    console.log('jj');
+    
+}
+
+try {
+    let toLoginPage = document.getElementById('to-login-page')
+    toLoginPage.addEventListener('click',(e)=>{
+    window.location.href = './login.html'
+})
+} catch (error) {
+    
+}
+
+try {
+    let toIndexPage = document.getElementById('to-index-page')
+    toIndexPage.addEventListener('click',(e)=>{
+    window.location.href = './index.html';
+        sessionStorage.setItem('fromLogin', 'true');
+})
+} catch (error) {
+}
+
+try {
+    let toverificationPage = document.getElementById('to-verification-page')
+    toverificationPage.addEventListener('click',(e)=>{
+    window.location.href = './verification.html'
+})
+} catch (error) {
+    
+}
+
+
+window.onload = function() {
+    if (sessionStorage.getItem('fromLogin') === 'true') {
+        let loginBtn = document.getElementById('login-btn');
+        if (loginBtn) {
+            loginBtn.remove();
+            document.getElementById('btn-image-replace').innerHTML = `<p class="recoloring">Anna Liebert</p> <img src="./assets/Ellipse 12.png" alt="">`
+    let toinfoPage = document.getElementById('btn-image-replace')
+    toinfoPage.addEventListener('click',(e)=>{
+    window.location.href = './personalInfo.html'
+})
+
+    
+        }
+        sessionStorage.removeItem('fromLogin'); 
+    }
+};
+function previewImage(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profile-preview').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}
+document.getElementById("contact-num").addEventListener("input", function (event) {
+    let inputValue = event.target.value.replace(`${/\D/g}`, ""); 
+
+    // Ensure it starts with "09"
+    if (!inputValue.startsWith("09")) {
+        inputValue = "09"; // Force the input to begin with 09
+    }
+
+    // Limit to exactly 10 digits
+    inputValue = inputValue.slice(0, 10);
+
+    event.target.value = inputValue;
+});
 
